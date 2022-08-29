d10f4a7223596fbdfced624e770124f6d820c7db
Chi Tse <chiyantsestudio@gmail.com>
2022-08-29 15:43:31 -0400

    finish 13.11 studio Spay Day!

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -24,6 +24,7 @@ public class SpaDayController {
         }
     }
 
+
     @GetMapping(value="")
     @ResponseBody
     public String customerForm () {
@@ -62,6 +63,12 @@ public class SpaDayController {
                 appropriateFacials.add(facials.get(i));
             }
         }
+
+
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
+        model.addAttribute("appropriateFacials", appropriateFacials);
 
         return "menu";
     }
