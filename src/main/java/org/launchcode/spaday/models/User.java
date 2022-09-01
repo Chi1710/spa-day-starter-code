63bf93626a6ce955bd25e1e1deb117dc0fd2dacf
Chi Tse <chiyantsestudio@gmail.com>
2022-09-01 00:43:27 -0400

    finish 14.6 studio Spay User Signup

---
+++ src/main/java/org/launchcode/spaday/models/User.java
@@ -0,0 +1,38 @@
+package org.launchcode.spaday.models;
+
+public class User {
+
+    private String username;
+    private String email;
+    private String password;
+
+    public User(String username, String email, String password) {
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return username;
+    }
+
+    public void setUsersName(String userName) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
